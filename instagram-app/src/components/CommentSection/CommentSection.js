750088da3ca1d5af202d27541c441eee0934f357
sam-malanchuk <malanchuksam@gmail.com>
2019-06-12 22:03:52 -0400

    added instagram favicon and updated page title

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CommentSection.css';
+
+function commentSection(props) {
+    return (
+        <div className="commentSection">
+        </div>
+    );
+}
+
+export default CommentSection;

bacbbb20510b2efe882dab4967e6322dc2f1c61c
sam-malanchuk <malanchuksam@gmail.com>
2019-06-12 21:29:39 -0400

    created the component and styling for PostContainer without completing the CommentSection component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,16 +3,20 @@ import './PostContainer.css';
 
 function PostContainer(props) {
     return (
-        <div>
-            <div className="postTop">
-                <img src={props.postData.thumbnailUrl} className="profileImg" alt={props.postData.username + "'s profile image"} />
-                <p>{props.postData.username}</p>
+        <div className="postContainer">
+            <div className="postTopContainer">
+                <a href={"https://www.instagram.com/" + props.postData.username + "/"}><img src={props.postData.thumbnailUrl} className="profileImg" alt={props.postData.username + "'s profile image"} /></a>
+                <a href={"https://www.instagram.com/" + props.postData.username + "/"} className="usernameText"><p>{props.postData.username}</p></a>
             </div>
-            <div className="postImg">
-                <img src={props.postData.imageUrl} alt="none" />
+            <div className="postImgContainer">
+                <img src={props.postData.imageUrl} className="postImg" alt={"image-" + props.postData.id} />
             </div>
-            <div className="postComments">
-                <p>{props.postData.likes}</p>
+            <div className="postCommentsContainer">
+                <div className="postIcons">
+                    <a href="/"><div className="postIcon heartIcon"></div></a>
+                    <a href="/"><div className="postIcon commentIcon"></div></a>
+                </div>
+                <p>{props.postData.likes} likes</p>
                 <p>{props.postData.timestamp}</p>
             </div>
         </div>
